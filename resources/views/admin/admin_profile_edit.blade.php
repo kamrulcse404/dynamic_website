c7310768978fea6ccbcd7ad9de116448e912af40
kamrulcse404 <kamrul.h01960@gmail.com>
2022-12-09 06:27:24 +0600

    admin profile & image update part 2

---
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -0,0 +1,67 @@
+@extends('admin.admin_master')
+@section('admin')
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                            <form action="">
+                                <h4 class="card-title">Edit Profile</h4>
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Name</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" name="name" type="text" id="example-text-input"
+                                            value="{{ $editData->name }}">
+                                    </div>
+                                </div>
+                                <!-- end row -->
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">User Email</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" name="email" type="text" id="example-text-input"
+                                            value="{{ $editData->email }}">
+                                    </div>
+                                </div>
+                                <!-- end row -->
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">User Name</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" name="username" type="text" id="example-text-input"
+                                            value="{{ $editData->username }}">
+                                    </div>
+                                </div>
+                                <!-- end row -->
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" name="profile_image" type="file"
+                                            id="example-text-input">
+                                    </div>
+                                </div>
+                                <!-- end row -->
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label"></label>
+                                    <div class="col-sm-10">
+                                        <img class="rounded avatar-lg"
+                                            src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                    </div>
+                                </div>
+                                <!-- end row -->
+
+                                <input type="submit" class="btn btn-info btn-rounded waves-effect" value="Update Profile">
+                            </form>
+
+                        </div>
+                    </div>
+                </div> <!-- end col -->
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -14,7 +14,7 @@
                             <h4 class="card-title">Name : {{ $adminData->name }}</h4><hr>
                             <h4 class="card-title">User Email : {{ $adminData->email }}</h4><hr>
                             <h4 class="card-title">User Name : {{ $adminData->username }}</h4><hr>
-                            <a href="" class="btn btn-info btn-rounded waves-effect">Edit Profile</a>
+                            <a href="{{ route('edit.profile') }}" class="btn btn-info btn-rounded waves-effect">Edit Profile</a>
                         </div>
                     </div>
                 </div>
