74913c5506dadd405a61af776b3447b7830b7add
kamrulcse404 <kamrul.h01960@gmail.com>
2022-12-13 01:21:55 +0600

    display upload image in profile, edit & header

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -53,7 +53,7 @@
                                     <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                                     <div class="col-sm-10">
                                         <img id="showImage" class="rounded avatar-lg"
-                                            src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                        src="{{ (!empty($editData->profile_image)) ? url('upload/admin_images/'.$editData->profile_image):url('upload/no_image.jpg') }}" alt="Card image cap">
                                     </div>
                                 </div>
                                 <!-- end row -->
